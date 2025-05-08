3b31a662bbcb118afeabbea0e91ca5cbc19e13a2
Tarbat <vitaliy.paymuhin@mail.ru>
2025-05-08 11:30:48 +0300

    Fixed an issue with not capturing tests

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -4,22 +4,22 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using cli_life;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Test
 {
+    [TestClass]
     public class LifeTests
     {
-        [Fact]
+        [TestMethod]
         public void Cell_Creation_IsInitiallyDead()
         {
             var cell = new Cell();
-            Assert.False(cell.IsAlive);
+            Assert.IsFalse(cell.IsAlive);
         }
 
-        [Fact]
+        [TestMethod]
         public void Cell_DetermineNextState_AliveWith4Neighbors_Dies()
         {
             var cell = new Cell { IsAlive = true };
@@ -27,27 +27,27 @@ namespace Test
                 cell.neighbors.Add(new Cell { IsAlive = true });
 
             cell.DetermineNextLiveState();
-            Assert.False(cell.IsAliveNext);
+            Assert.IsFalse(cell.IsAliveNext);
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_Creation_HasCorrectDimensions()
         {
             var board = new Board(100, 100, 10);
-            Assert.Equal(10, board.Columns);
-            Assert.Equal(10, board.Rows);
+            Assert.AreEqual(10, board.Columns);
+            Assert.AreEqual(10, board.Rows);
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_CountAliveCells_ReturnsCorrectCount()
         {
             var board = new Board(10, 10, 1, 0);
             board.Cells[0, 0].IsAlive = true;
             board.Cells[1, 1].IsAlive = true;
-            Assert.Equal(2, board.CountAliveCells());
+            Assert.AreEqual(2, board.CountAliveCells());
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_SaveAndLoad_StatePreserved()
         {
             var board1 = new Board(10, 10, 1);
@@ -56,40 +56,38 @@ namespace Test
 
             var board2 = new Board(10, 10, 1);
             board2.LoadFromFile("test_save.txt");
-            Assert.True(board2.Cells[0, 0].IsAlive);
+            Assert.IsTrue(board2.Cells[0, 0].IsAlive);
 
             File.Delete("test_save.txt");
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_Randomize_HasApproximateDensity()
         {
             double density = 0.3;
             var board = new Board(100, 100, 1, density);
             int alive = board.CountAliveCells();
             double actualDensity = alive / (double)(board.Columns * board.Rows);
-            Assert.InRange(actualDensity, density - 0.1, density + 0.1); // ±10%
+            Assert.IsTrue(actualDensity >= density - 0.1 && actualDensity <= density + 0.1);
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_ConnectNeighbors_CorrectCount()
         {
             var board = new Board(10, 10, 1);
-            Assert.Equal(8, board.Cells[0, 0].neighbors.Count); // Угловая клетка
-            Assert.Equal(8, board.Cells[5, 5].neighbors.Count); // Центральная клетка
+            Assert.AreEqual(8, board.Cells[0, 0].neighbors.Count);
+            Assert.AreEqual(8, board.Cells[5, 5].neighbors.Count);
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_Toroidal_WrappingWorks()
         {
             var board = new Board(10, 10, 1);
-            // Левая граница должна соединяться с правой
-            Assert.Contains(board.Cells[9, 0], board.Cells[0, 0].neighbors);
-            // Верхняя граница должна соединяться с нижней
-            Assert.Contains(board.Cells[0, 9], board.Cells[0, 0].neighbors);
+            CollectionAssert.Contains(board.Cells[0, 0].neighbors, board.Cells[9, 0]);
+            CollectionAssert.Contains(board.Cells[0, 0].neighbors, board.Cells[0, 9]);
         }
-       
-        [Fact]
+
+        [TestMethod]
         public void Cell_DetermineNextState_AliveWith2Neighbors_StaysAlive()
         {
             var cell = new Cell { IsAlive = true };
@@ -97,60 +95,71 @@ namespace Test
                 cell.neighbors.Add(new Cell { IsAlive = true });
 
             cell.DetermineNextLiveState();
-            Assert.True(cell.IsAliveNext);
+            Assert.IsTrue(cell.IsAliveNext);
         }
 
-        [Fact]
+        [TestMethod]
         public void Cell_DetermineNextState_AliveWith1Neighbor_Dies()
         {
             var cell = new Cell { IsAlive = true };
             cell.neighbors.Add(new Cell { IsAlive = true });
 
             cell.DetermineNextLiveState();
-            Assert.False(cell.IsAliveNext);
+            Assert.IsFalse(cell.IsAliveNext);
         }
-       
-        [Fact]
+
+        [TestMethod]
         public void Board_Empty_NoAliveCells()
         {
             var board = new Board(10, 10, 1);
             foreach (var cell in board.Cells)
                 cell.IsAlive = false;
 
-            Assert.Equal(0, board.CountAliveCells());
+            Assert.AreEqual(0, board.CountAliveCells());
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_Full_AllCellsAlive()
         {
             var board = new Board(10, 10, 1);
             foreach (var cell in board.Cells)
                 cell.IsAlive = true;
 
-            Assert.Equal(100, board.CountAliveCells());
+            Assert.AreEqual(100, board.CountAliveCells());
         }
-        
-        [Fact]
+
+        [TestMethod]
         public void Board_LoadNonexistentFile_HandlesGracefully()
         {
             var board = new Board(10, 10, 1);
-            var exception = Record.Exception(() => board.LoadFromFile("nonexistent.txt"));
-            Assert.Null(exception);
+            try
+            {
+                board.LoadFromFile("nonexistent.txt");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Expected no exception, but got {ex.GetType().Name}");
+            }
         }
 
-        [Fact]
+        [TestMethod]
         public void Board_SaveToInvalidPath_HandlesGracefully()
         {
             var board = new Board(10, 10, 1);
-            var exception = Record.Exception(() => board.SaveToFile("/invalid/path/save.txt"));
-            Assert.Null(exception);
+            try
+            {
+                board.SaveToFile("/invalid/path/save.txt");
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"Expected no exception, but got {ex.GetType().Name}");
+            }
         }
 
-        [Fact]
+        [TestMethod]
         public void Bloc_MovesCorrectly()
         {
             var board = new Board(10, 10, 1, 0);
-            // Создаем блок
             board.Cells[2, 1].IsAlive = true;
             board.Cells[3, 1].IsAlive = true;
             board.Cells[2, 0].IsAlive = true;
@@ -158,7 +167,7 @@ namespace Test
 
             int initialCount = board.CountAliveCells();
             for (int i = 0; i < 4; i++) board.Advance();
-            Assert.Equal(initialCount, board.CountAliveCells()); // Количество клеток не меняется
+            Assert.AreEqual(initialCount, board.CountAliveCells());
         }
     }
 }
--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2bedf48c35ef3744417b3b5f8f192d570637650")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f8bef8bfdbbfe7a40dee1325998b19fe23aa2c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
